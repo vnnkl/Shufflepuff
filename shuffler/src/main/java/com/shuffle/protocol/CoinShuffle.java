02f67a121c5ce72d7c4ce69bf638981fc2601ff5
Constantin Vennekel <constantin.vennekel@mycelium.com>
2016-06-22 18:53:41 +0200

    cleaner PR

--- shuffler/src/main/java/com/shuffle/protocol/CoinShuffle.java
+++ shuffler/src/main/java/com/shuffle/protocol/CoinShuffle.java
@@ -29,8 +29,8 @@ import com.shuffle.protocol.message.MessageFactory;
 import com.shuffle.protocol.message.Packet;
 import com.shuffle.protocol.message.Phase;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import java.io.IOException;
 import java.util.Deque;
