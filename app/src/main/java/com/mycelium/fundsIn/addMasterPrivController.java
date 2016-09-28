7a1d10b287b1bfd3f2f63b329e479b2db340614a
vnnkl <cvennekel+github@gmail.com>
2016-09-28 15:59:30 +0200

    - add MasterPriv screen working - wording

--- app/src/main/java/com/mycelium/fundsIn/addMasterPrivController.java
+++ app/src/main/java/com/mycelium/fundsIn/addMasterPrivController.java
@@ -17,6 +17,9 @@
 package com.mycelium.fundsIn;
 
 import com.mycelium.Main;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -27,15 +30,16 @@ import java.util.ArrayList;
 public class addMasterPrivController {
     public Button AddBtn;
     public Button cancelBtn;
-    public TextField inputPrivKEdit;
-    public TextField inputIndexEdit;
-    public ArrayList<String> privKeyList;
+    public TextField inputMasterPrivEdit;
+    public ArrayList<String> privKeyList = new ArrayList<String>(1);
+    ListProperty<String> listProperty = new SimpleListProperty<>();
     public ListView privKeyListView;
     public Main.OverlayUI overlayUI;
 
 
     // Called by FXMLLoader
     public void initialize() {
+        privKeyListView.itemsProperty().bind(listProperty);
     }
 
     public void cancel(ActionEvent event) {
@@ -43,60 +47,18 @@ public class addMasterPrivController {
     }
 
     public void addInput(ActionEvent event) {
-        // add Input, could be invalid still
-            String newInput = inputPrivKEdit.getText();
-            privKeyList.add(newInput);
-
-
-
-            /**SendRequest req;
-            if (amount.equals(Main.bitcoin.wallet().getBalance()))
-                req = SendRequest.emptyWallet(destination);
-
-            else
-                req = SendRequest.to(destination, amount);
-            req.aesKey = aesKey;
-            sendResult = Main.bitcoin.wallet().sendCoins(req);
-            Futures.addCallback(sendResult.broadcastComplete, new FutureCallback<Transaction>() {
-                @Override
-                public void onSuccess(@Nullable Transaction result) {
-                    checkGuiThread();
-                    overlayUI.done();
-                }
-
-                @Override
-                public void onFailure(Throwable t) {
-                    // We died trying to empty the wallet.
-                    crashAlert(t);
-                }
-            });
-            sendResult.tx.getConfidence().addEventListener((tx, reason) -> {
-                if (reason == TransactionConfidence.Listener.ChangeReason.SEEN_PEERS)
-                    updateTitleForBroadcast();
-            });
-            AddBtn.setDisable(true);
-            address.setDisable(true);
-            ((HBox)amountEdit.getParent()).getChildren().remove(amountEdit);
-            ((HBox)btcLabel.getParent()).getChildren().remove(btcLabel);
-            updateTitleForBroadcast();
-             **/
-
-    }
-
-    private void askForPasswordAndRetry() {
-        /**Main.OverlayUI<WalletPasswordController> pwd = Main.instance.overlayUI("wallet_password.fxml");
-        final String addressStr = address.getText();
-        final String amountStr = amountEdit.getText();
-        pwd.controller.aesKeyProperty().addListener((observable, old, cur) -> {
-            // We only get here if the user found the right password. If they don't or they cancel, we end up back on
-            // the main UI screen. By now the send money screen is history so we must recreate it.
-            checkGuiThread();
-            Main.OverlayUI<addUTXOController> screen = Main.instance.overlayUI("send_money.fxml");
-            screen.controller.aesKey = cur;
-            screen.controller.address.setText(addressStr);
-            screen.controller.amountEdit.setText(amountStr);
-            screen.controller.send(null);
-        });**/
+        // add MasterPriv, could be invalid still
+        // todo: check input for being valid MasterPrivKey
+        String newInput = inputMasterPrivEdit.getText();
+        // get rid of possible blanks like used in prompt text and classic wallet
+        String betterInput = newInput.replaceAll(" ","");
+        // Limit entries to List to 1, not more MasterPrivKeys needed
+        if (privKeyList.size()<1) {
+            if (!privKeyList.contains(betterInput)) {
+                privKeyList.add(betterInput);
+            }
+        }
+        listProperty.set(FXCollections.observableArrayList(privKeyList));
     }
 
 
