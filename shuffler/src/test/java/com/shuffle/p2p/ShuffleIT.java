42ad65a5437eb27e2866574879a5ab9ee83ea519
vnnkl <conta@Constantins-iMac.local>
2016-12-19 13:14:23 +0100

    - optionSet

--- shuffler/src/test/java/com/shuffle/p2p/ShuffleIT.java
+++ shuffler/src/test/java/com/shuffle/p2p/ShuffleIT.java
@@ -1,16 +1,29 @@
 package com.shuffle.p2p;
 
+import com.neemre.btcdcli4j.core.BitcoindException;
+import com.neemre.btcdcli4j.core.CommunicationException;
+import com.shuffle.bitcoin.impl.BitcoinCrypto;
 import com.shuffle.player.Shuffle;
+import com.shuffle.protocol.FormatException;
 
+import org.bitcoinj.core.AddressFormatException;
 import org.bitcoinj.core.Coin;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.NetworkParameters;
+import org.json.JSONArray;
+import org.junit.Assert;
+import org.junit.Test;
 
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
 /**
  * Created by conta on 08.12.16.
@@ -53,36 +66,57 @@ public class ShuffleIT {
 
 
    // prepare optionSets for shuffle
-   public OptionParser getOptionParser() {
-      OptionParser optionParser = new OptionParser();
-      optionParser.accepts("session").withRequiredArg();
-      optionParser.accepts("amount").withRequiredArg();
-      optionParser.accepts("time").withRequiredArg();
-      optionParser.accepts("fee").withRequiredArg();
-
-      optionParser.accepts("port");
-      optionParser.accepts("key");
-      optionParser.accepts("anon");
-      optionParser.accepts("change");
-
-      optionParser.accepts("query").withOptionalArg();
-      optionParser.accepts("blockchain");
-      optionParser.accepts("local");
-      optionParser.accepts("peers");
-
-      optionParser.accepts("rpcuser").requiredIf("query").withRequiredArg();
-      optionParser.accepts("rpcpass").requiredIf("query").withRequiredArg();
-
-
-      return optionParser;
+   public static OptionParser getShuffleOptionsParser() {
+      OptionParser parser = Shuffle.getShuffleOptionsParser();
+      return parser;
    }
-
 
    // OptionSet options = parser.parse( "-a", "-B", "-?" );
 
+   OptionSet testOptions;
 
+   @Test
+   public void testOptionParser() {
+      String arguments = "--amount 5000000 --session testnet00 --query bitcoin-core --blockchain test  --key cPeH8CS8TGKK29yKAFB4K4boDBXK3wV4fZTwMh15HDhiYdYJ9TGK --anon n3izGxHXfgafbhtQ8Ls8CMsqFNbYcMncM3 --change moFLhtjSQjiTxCYetVcJpdEZoKob4vWCNV --port 3001 --utxos [{\"vout\":\"3\",\"transactionHash\":\"0d41246259b95010bac124b3f0cc5c4d98f0449eef5d533f5af39e9b86740cb4\"}] --fee 8000 --time " + time;
+      String[] splitted = arguments.split(" ");
+      OptionSet options = getShuffleOptionsParser().parse(splitted);
+      Assert.assertTrue(options.has("amount"));
+      Assert.assertTrue(options.has("session"));
+      Assert.assertTrue(options.valueOf("session").toString().equals("testnet00"));
+      Assert.assertTrue(options.valueOf("query").toString().equals("bitcoin-core"));
+      Assert.assertTrue(options.valueOf("blockchain").toString().equals("test"));
+      Assert.assertTrue(options.valueOf("key").toString().equals("cPeH8CS8TGKK29yKAFB4K4boDBXK3wV4fZTwMh15HDhiYdYJ9TGK"));
+      Assert.assertTrue(options.valueOf("anon").toString().equals("n3izGxHXfgafbhtQ8Ls8CMsqFNbYcMncM3"));
+      Assert.assertTrue(options.valueOf("change").toString().equals("moFLhtjSQjiTxCYetVcJpdEZoKob4vWCNV"));
+      Assert.assertTrue(options.valueOf("fee").toString().equals("8000"));
+
+      // print utxo
+      JSONArray array = new JSONArray(options.valueOf("u").toString());
+      System.out.print(array.toString());
+      testOptions = getShuffleOptionsParser().parse(splitted);
+   }
    // new shuffle
-   Shuffle shuffle1 = new Shuffle();
+   @Test
+   public void testShuffle() {
+      testOptionParser();
+      try {
+         Shuffle testShuffle = new Shuffle(testOptions, System.out);
+      } catch (ParseException | UnknownHostException | FormatException e) {
+         e.printStackTrace();
+      } catch (NoSuchAlgorithmException e) {
+         e.printStackTrace();
+      } catch (AddressFormatException e) {
+         e.printStackTrace();
+      } catch (MalformedURLException e) {
+         e.printStackTrace();
+      } catch (BitcoinCrypto.Exception e) {
+         e.printStackTrace();
+      } catch (BitcoindException e) {
+         e.printStackTrace();
+      } catch (CommunicationException e) {
+         e.printStackTrace();
+      }
+   }
 
 
 
@@ -116,7 +150,7 @@ public class ShuffleIT {
 
    public List<String> getPorts(Integer n) {
       List<String> portStringList = new LinkedList<>();
-      for (int i = 1880; i < i + n; i++) {
+      for (int i = 1880; i < (1880 + n); i++) {
          portStringList.add(i - 1880, String.valueOf(i));
       }
       return portStringList;
