be2beee64923cc79bea814f3d9b910caef62e8e0
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2017-01-13 09:30:17 -0600

    BasicChan can now have a capacity of zero.

--- shuffler/src/main/java/com/shuffle/chan/BasicChan.java
+++ shuffler/src/main/java/com/shuffle/chan/BasicChan.java
@@ -39,20 +39,36 @@ public class BasicChan<X> implements Chan<X> {
     private boolean closed = false;
     private boolean closeSent = false;
     private final LinkedBlockingQueue<Message> q;
+    private final int cap;
 
     private final Object lock = new Object();
 
-    public BasicChan(int n) {
-        if (n < 1) throw new IllegalArgumentException();
+    public BasicChan(int cap) {
+        if (cap < 0) throw new IllegalArgumentException();
 
-        q = new LinkedBlockingQueue<>(n);
+        this.cap = cap;
+        if (cap == 0) {
+            // Since LinkedBlockingQueues must have a capacity of at least 1,
+            // if cap is set to zero, we just keep the queue filled up with
+            // an empty message. 
+            q = new LinkedBlockingQueue<>(1);
+            try {
+                q.put(new Message());
+            } catch (InterruptedException e) {
+                throw new RuntimeException("This shouldn't happen.");
+            }
+        } else {
+            q = new LinkedBlockingQueue<>(cap);
+        }
     }
 
     public BasicChan() {
         q = new LinkedBlockingQueue<>(1);
+        cap = 1;
     }
 
-    private synchronized X receiveMessage(Message m) {
+    private X receiveMessage(Message m) {
+
         if (closed && !closeSent) {
             // There is definitely room in the queue because we just removed
             // one element and no more were allowed to be put in.
@@ -70,7 +86,12 @@ public class BasicChan<X> implements Chan<X> {
             return null;
         }
 
-        return receiveMessage(q.take());
+        synchronized (lock) {
+            if (cap == 0) {
+                q.take();
+            }
+            return receiveMessage(q.take());
+        }
     }
 
     @Override
@@ -79,7 +100,12 @@ public class BasicChan<X> implements Chan<X> {
             return null;
         }
 
-        return receiveMessage(q.poll(l, u));
+        synchronized (lock) {
+            if (cap == 0) {
+                q.take();
+            }
+            return receiveMessage(q.poll(l, u));
+        }
     }
 
     @Override
@@ -91,6 +117,10 @@ public class BasicChan<X> implements Chan<X> {
         if (closed) return false;
 
         q.put(new Message(x));
+
+        if (cap == 0) {
+            q.put(new Message());
+        }
 
         return true;
     }
