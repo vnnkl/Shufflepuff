71914e2b90fa25cd12a08601efe381076b5175c6
nsa <elziegel@gmail.com>
2017-01-23 16:20:27 -0500

    build script

---
+++ build_bc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for (( c=1; ;c++ ))
+do
+  result=$(grep "security.provider.$c" "$JAVA_HOME/jre/lib/security/java.security")
+  if [ "$result" ]
+    then
+      continue
+  fi
+  echo "security.provider.$c=org.bouncycastle.jce.provider.BouncyCastleProvider" >> "$JAVA_HOME/jre/lib/security/java.security"
+  break
+done
+
+$(curl -o "$JAVA_HOME/jre/lib/ext/bcprov-jdk15on-155.jar" http://www.bouncycastle.org/download/bcprov-jdk15on-155.jar)
